0696d9dae47db849a0f30da3bbf49ff1a01e9911
mohammedSiam72 <124588293+mohammedSiam72@users.noreply.github.com>
2023-03-05 18:41:45 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".email-sec form input").click(function(){
+        $(".email-sec form  input").css("background" , "#dddddd");
+    })
+
+    $(".email-sec form input").hover(function(){
+        $(".email-sec form input").css("background" , "#fff")
+    })
+  
+})
